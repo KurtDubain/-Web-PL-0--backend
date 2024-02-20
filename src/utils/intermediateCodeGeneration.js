c086cdec49cb127da6df2ffa60f907c478ade2d9
Kurt.Dubain <kurt.du.cobain@gmail.com>
2024-02-20 17:35:11 +0800

    修改了中间代码生成部分的内容

--- src/utils/intermediateCodeGeneration.js
+++ src/utils/intermediateCodeGeneration.js
@@ -11,18 +11,15 @@ const intermediateCodeGenerator = {
     switch (node.type) {
       case 'Program':
       case 'Block':
-        // 对于Block类型，我们需要遍历其所有声明和语句
         node.children?.forEach(child => this.generateCodeFromNode(child, intermediateCode));
         break;
 
       case 'BeginEndBlock':
-        // BeginEndBlock可能直接包含一系列语句
         node.statements.forEach(statement => this.generateCodeFromNode(statement, intermediateCode));
         break;
 
       case 'Declaration':
         node.children.forEach(decl => {
-          // 处理变量和常量声明
           if (decl.type === 'VarDeclaration') {
             intermediateCode.push(`DECLARE ${decl.name}`);
           } else if (decl.type === 'ConstDeclaration') {
@@ -32,47 +29,55 @@ const intermediateCodeGenerator = {
         break;
 
       case 'ProcedureDeclaration':
-        // 进入和离开过程声明
         intermediateCode.push(`PROCEDURE ${node.name} START`);
         this.generateCodeFromNode(node.body, intermediateCode);
         intermediateCode.push(`PROCEDURE ${node.name} END`);
         break;
 
       case 'AssignmentStatement':
-        // 生成赋值语句的中间代码
         this.generateCodeFromNode(node.expression, intermediateCode);
         intermediateCode.push(`STORE ${node.identifier}`);
         break;
 
+      case 'ProcedureCall':
+        intermediateCode.push(`CALL ${node.name}`);
+        break;
+
+      case 'ReadStatement':
+        intermediateCode.push(`READ ${node.variableName}`);
+        break;
+
+      case 'WriteStatement':
+        this.generateCodeFromNode(node.expression, intermediateCode);
+        intermediateCode.push(`WRITE`);
+        break;
+
       case 'IfStatement':
-        // 生成条件判断的中间代码
-        this.generateExpressionCode(node.condition, intermediateCode);
-                intermediateCode.push('IF');
-                this.generateCodeFromNode(node.thenStatement, intermediateCode);
-                if (node.elseIfStatement) {
-                    node.elseIfStatement.forEach(elseif => {
-                        intermediateCode.push('ELSEIF');
-                        this.generateCodeFromNode(elseif.condition, intermediateCode);
-                        this.generateCodeFromNode(elseif.thenStatement, intermediateCode);
-                    });
-                }
-                if (node.elseStatement) {
-                    intermediateCode.push('ELSE');
-                    this.generateCodeFromNode(node.elseStatement, intermediateCode);
-                }
-                intermediateCode.push('ENDIF');
-                break;
+        this.generateCodeFromNode(node.condition, intermediateCode);
+        intermediateCode.push('IF');
+        this.generateCodeFromNode(node.thenStatement, intermediateCode);
+        if (node.elseIfStatement && node.elseIfStatement.length > 0) {
+            node.elseIfStatement.forEach(elseif => {
+                intermediateCode.push('ELSEIF');
+                this.generateCodeFromNode(elseif.condition, intermediateCode);
+                this.generateCodeFromNode(elseif.thenStatement, intermediateCode);
+            });
+        }
+        if (node.elseStatement) {
+            intermediateCode.push('ELSE');
+            this.generateCodeFromNode(node.elseStatement, intermediateCode);
+        }
+        intermediateCode.push('ENDIF');
+        break;
 
       case 'WhileStatement':
-        // 生成while循环的中间代码
-        this.generateCodeFromNode(node.condition, intermediateCode);
         intermediateCode.push('WHILE');
+        this.generateCodeFromNode(node.condition, intermediateCode);
         this.generateCodeFromNode(node.body, intermediateCode);
         intermediateCode.push('ENDWHILE');
         break;
 
       case 'ForStatement':
-        // 生成for循环的中间代码
         this.generateCodeFromNode(node.initialValue, intermediateCode);
         intermediateCode.push(`FOR ${node.variableName} INIT`);
         this.generateCodeFromNode(node.finalValue, intermediateCode);
@@ -96,33 +101,12 @@ const intermediateCodeGenerator = {
         intermediateCode.push(`LOAD ${node.name}`);
         break;
 
-      case 'ProcedureCall':
-        intermediateCode.push(`CALL ${node.name}`);
-        break;
-
       // 添加对其他节点类型的处理...
 
       default:
         console.warn(`Unhandled node type: ${node.type}`);
     }
-  },
-  generateExpressionCode(node, intermediateCode) {
-    switch (node.type) {
-        case 'BinaryExpression':
-            this.generateCodeFromNode(node.left, intermediateCode);
-            this.generateCodeFromNode(node.right, intermediateCode);
-            intermediateCode.push(`OPER ${node.operator}`);
-            break;
-        case 'Literal':
-            intermediateCode.push(`PUSH ${node.value}`);
-            break;
-        case 'Identifier':
-            intermediateCode.push(`LOAD ${node.name}`);
-            break;
-        default:
-            console.warn(`Unhandled expression type: ${node.type}`);
-    }
-}
+  }
 };
 
 module.exports = intermediateCodeGenerator;
